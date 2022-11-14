4bbeb1fab810df86f40a96e941c1b4ee49535324
DBA <7777_7788@mail.ru>
2022-11-14 10:46:19 +0300

    hw_01 and hw_02

---
+++ HomeWork_14/db_connection.py
@@ -0,0 +1,7 @@
+import peewee
+
+db = peewee.MySQLDatabase("homeWork_14",
+                          user="root",
+                          password="qwerty",
+                          host="localhost",
+                          port=3306)
